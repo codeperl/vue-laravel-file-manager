a544f1fb94a8b61edba196e959798ac2c42793e8
Mohammad Ashraufuddin Ferdousi <ashferd.business@gmail.com>
2022-11-17 17:51:50 +0600

    Fixing settings bug and update version to 3.0.4

--- src/http/axios.js
+++ src/http/axios.js
@@ -4,8 +4,10 @@ import localstore from '../mixins/localstore';
 let settings = localstore.getStorage(localstore.axiosSettingType);
 let obj = {};
 
-if(settings.data.baseURL)  obj.baseURL = settings.data.baseURL;
-if(settings.data.headers)  obj.headers = settings.data.headers;
+if(settings) {
+    if(settings.data.baseURL)  obj.baseURL = settings.data.baseURL;
+    if(settings.data.headers)  obj.headers = settings.data.headers;
+}
 
 // set new axios instance
 export default axios.create(obj);
