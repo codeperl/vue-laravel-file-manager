3e939fb0345bcb44d188228c7d6eaab01b2e90b8
Mohammad Ashraufuddin Ferdousi <ashferd.business@gmail.com>
2022-11-21 01:53:48 +0600

    Updated.

--- src/http/axios.js
+++ src/http/axios.js
@@ -4,9 +4,9 @@ import localstore from '../mixins/localstore';
 let settings = localstore.getStorage(localstore.axiosSettingType);
 
 if(settings) {
-    if(settings.baseURL)  axios.default.baseURL = settings.baseURL;
-    if(settings.withCredentials)  axios.default.withCredentials = settings.withCredentials;
-    if(settings.headers)  axios.default.headers = settings.headers;
+    if(settings.baseURL)  axios.defaults.baseURL = settings.baseURL;
+    if(settings.withCredentials)  axios.defaults.withCredentials = settings.withCredentials;
+    if(settings.headers)  axios.defaults.headers = settings.headers;
 }
 
 // set new axios instance
